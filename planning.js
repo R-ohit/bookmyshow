53eeb41751a4858d43638c650e3b9571dcb83bbd
R-ohit <rohitkaurav16@gmail.com>
2021-10-12 17:47:45 +0530

    book my show

---
+++ planning.js
@@ -0,0 +1,24 @@
+/* BOOK MY SHOW planning 
+
+ -> we need a reusable carousel 
+         -> hero carousel
+         -> common carousel
+         -> cast and crew small carousel
+
+
+ Pages 
+ -> Home page 
+ -> movie page
+ -> categories page
+ -> auth page 
+ -> razorpay payment gateway
+
+ Plan -> how are we building it??? 
+ layouts 
+ routes
+ pages 
+ components
+
+
+
+ HOC -> higher order components (return a funtion), used to handle layouts */
